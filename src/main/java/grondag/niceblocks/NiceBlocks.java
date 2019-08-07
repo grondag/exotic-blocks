9163edcbdbfc58733b7e9f41f7be3230d2603bb1
grondag <grondag@gmail.com>
2019-08-07 12:07:23 -0700

    Custom texture layout map

--- src/main/java/grondag/niceblocks/NiceBlocks.java
+++ src/main/java/grondag/niceblocks/NiceBlocks.java
@@ -1,66 +1,101 @@
 package grondag.niceblocks;
 
-import grondag.fermion.color.Chroma;
-import grondag.fermion.color.Color;
-import grondag.fermion.color.Hue;
-import grondag.fermion.color.Luminance;
+import static grondag.xm.api.texture.TextureGroup.STATIC_BORDERS;
+import static grondag.xm.api.texture.TextureRenderIntent.OVERLAY_ONLY;
+import static grondag.xm.api.texture.TextureRotation.ROTATE_NONE;
+import static grondag.xm.api.texture.TextureScale.SINGLE;
+
+import java.util.function.Function;
+
+import grondag.xm.api.block.XmBlockRegistry;
+import grondag.xm.api.connect.world.BlockTest;
+import grondag.xm.api.modelstate.ImmutableModelState;
 import grondag.xm.api.modelstate.OwnedModelState;
 import grondag.xm.api.paint.XmPaint;
-import grondag.xm.api.paint.XmPaintFinder;
-import grondag.xm.block.XmSimpleBlock;
+import grondag.xm.api.texture.TextureSet;
+import grondag.xm.block.WorldToModelStateFunction;
+import grondag.xm.block.XmBlockRegistryImpl.XmBlockStateImpl;
 import grondag.xm.init.XmPrimitives;
-import grondag.xm.init.XmTextures;
-import grondag.xm.placement.XmBlockItem;
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.api.block.FabricBlockSettings;
-import net.minecraft.block.Block;
 import net.minecraft.block.BlockRenderLayer;
-import net.minecraft.block.Material;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroup;
-import net.minecraft.util.Identifier;
-import net.minecraft.util.registry.Registry;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
 
 public class NiceBlocks implements ModInitializer {
     static final String MODID = "niceblocks";
     @Override
     public void onInitialize() {
+        borderedGlass();
         initSimpleBlocks();
     }
     
     private static final void initSimpleBlocks() {
+  
+//        WorldToModelStateFunction worldStateFunc,
+//        BlockTest blockJoinTest
+        
+//        model = XmPrimitives.WEDGE.newState();
+//        model.paintAll(paint);
+//        register(new XmSimpleBlock(FabricBlockSettings.of(Material.STONE).strength(1, 1).build(), model), "test_wedge");
+//        model.release();
+//
+//        paint = paintFinder.texture(0, XmTextures.WHITE).textureColor(0, 0xFFFFFFFF).find();
+//        model = XmPrimitives.CUBE.newState();
+//        model.paintAll(paint);
+//        register(new XmSimpleBlock(FabricBlockSettings.of(Material.STONE).strength(1, 1).build(), model), "test_cube");
+//        model.release();
+    }
 
-        final XmPaintFinder paintFinder = XmPaint.finder();
-        XmPaint paint = paintFinder.textureDepth(2)
-                .texture(0, XmTextures.SANDSTONE_ZOOM)
-                .textureColor(0, Color.fromHCL(Hue.AZURE, Chroma.NEUTRAL, Luminance.LIGHT).ARGB)
-                .texture(1, XmTextures.BORDER_SMOOTH_BLEND)
-                .blendMode(1, BlockRenderLayer.TRANSLUCENT)
-                .emissive(1, true)
-                .textureColor(1, 0xFFFFD300)
+//    private static void register(Block block, String name) {
+//        Identifier id = new Identifier(MODID, name);
+//        Registry.BLOCK.add(id, block);
+//        Registry.ITEM.add(id, new BlockItem(block, new Item.Settings().maxCount(64).group(ItemGroup.BUILDING_BLOCKS)));
+//    }
+
+    private static void borderedGlass() {
+        
+        final TextureSet fancyGlass = TextureSet.builder().displayNameToken("fancy_glass")
+                .baseTextureName("niceblocks:blocks/glass")
+                .versionCount(1)
+                .scale(SINGLE)
+                .layout(NiceTextureLayoutMap.BORDER_LAYOUT)
+                .rotation(ROTATE_NONE)
+                .renderIntent(OVERLAY_ONLY).groups(STATIC_BORDERS).build("niceblocks:fancy_glass");
+        
+        // Define our paint
+        final XmPaint paint = XmPaint.finder().textureDepth(1)
+                .texture(0, fancyGlass)
+                .blendMode(0, BlockRenderLayer.TRANSLUCENT)
+                .textureColor(0, 0xFFAAAAAA)
                 .find();
         
-        OwnedModelState model = XmPrimitives.CUBE.newState();
-        model.paintAll(paint);
-        register(new XmSimpleBlock(FabricBlockSettings.of(Material.STONE).strength(1, 1).build(), model), "test_borders");
-        model.release();
+        // Function to map block states to default model state
+        final Function<BlockState, ImmutableModelState> defaultStateFunc = b -> {
+            OwnedModelState ms = XmPrimitives.CUBE.newState();
+            ms.paintAll(paint);
+            return ms.releaseToImmutable();
+        };
         
-        model = XmPrimitives.WEDGE.newState();
-        model.paintAll(paint);
-        register(new XmSimpleBlock(FabricBlockSettings.of(Material.STONE).strength(1, 1).build(), model), "test_wedge");
-        model.release();
-
-        paint = paintFinder.texture(0, XmTextures.WHITE).textureColor(0, 0xFFFFFFFF).find();
-        model = XmPrimitives.CUBE.newState();
-        model.paintAll(paint);
-        register(new XmSimpleBlock(FabricBlockSettings.of(Material.STONE).strength(1, 1).build(), model), "test_cube");
-        model.release();
+        // Function to update model state from world state
+        WorldToModelStateFunction worldStateFunc = (xmBlockState, world, pos, refreshFromWorld) -> {
+            if (refreshFromWorld) {
+                OwnedModelState result = xmBlockState.defaultModelState().mutableCopy();
+                //TODO: refactor refresh mechanism before release
+                result.refreshFromWorld((XmBlockStateImpl) xmBlockState, world, pos);
+                return result.releaseToImmutable();
+            } else {
+                return xmBlockState.defaultModelState();
+            }
+        };
+        
+        // Function to determine if blocks are joined for connected textures/shapes
+        BlockTest blockJoinTest = ctx -> {
+            return ctx.fromBlockState().getBlock() == ctx.toBlockState().getBlock();
+        };
+        
+        
+        // Remap glass block using our new model
+        XmBlockRegistry.register(Blocks.GLASS, defaultStateFunc, worldStateFunc, blockJoinTest);
+        
     }
-
-    private static void register(Block block, String name) {
-        Identifier id = new Identifier(MODID, name);
-        Registry.BLOCK.add(id, block);
-        Registry.ITEM.add(id, new XmBlockItem(block, new Item.Settings().maxCount(64).group(ItemGroup.BUILDING_BLOCKS)));
-    }
-
 }
---
+++ src/main/java/grondag/niceblocks/NiceTextureLayoutMap.java
@@ -0,0 +1,45 @@
+package grondag.niceblocks;
+
+import static grondag.xm.api.texture.TextureNameFunction.BORDER_CORNERS_ALL;
+import static grondag.xm.api.texture.TextureNameFunction.BORDER_CORNERS_BL_TR;
+import static grondag.xm.api.texture.TextureNameFunction.BORDER_CORNERS_BL_TR_BR;
+import static grondag.xm.api.texture.TextureNameFunction.BORDER_CORNERS_TL_TR;
+import static grondag.xm.api.texture.TextureNameFunction.BORDER_CORNER_TR;
+import static grondag.xm.api.texture.TextureNameFunction.BORDER_MIXED_TOP_BL_BR;
+import static grondag.xm.api.texture.TextureNameFunction.BORDER_MIXED_TOP_BR;
+import static grondag.xm.api.texture.TextureNameFunction.BORDER_MIXED_TOP_RIGHT_BL;
+import static grondag.xm.api.texture.TextureNameFunction.BORDER_SIDES_ALL;
+import static grondag.xm.api.texture.TextureNameFunction.BORDER_SIDES_TOP_BOTTOM;
+import static grondag.xm.api.texture.TextureNameFunction.BORDER_SIDES_TOP_LEFT_RIGHT;
+import static grondag.xm.api.texture.TextureNameFunction.BORDER_SIDES_TOP_RIGHT;
+import static grondag.xm.api.texture.TextureNameFunction.BORDER_SIDE_TOP;
+
+import grondag.xm.api.texture.TextureLayout;
+import grondag.xm.api.texture.TextureLayoutMap;
+
+/**
+ * Example of a custom texture layout map.  Handles converting the texture
+ * ordinals expected by a texture layout to a physical file name.
+ */
+public class NiceTextureLayoutMap {
+    
+    private final static String[] BORDER_MAP = new String[TextureLayout.BORDER_13.textureCount];
+    
+    static {
+        BORDER_MAP[BORDER_SIDES_ALL] = "all_edges";
+        BORDER_MAP[BORDER_SIDE_TOP] = "top_edge";
+        BORDER_MAP[BORDER_SIDES_TOP_RIGHT] = "top_right_edges";
+        BORDER_MAP[BORDER_SIDES_TOP_BOTTOM] = "top_bottom_edges";
+        BORDER_MAP[BORDER_SIDES_TOP_LEFT_RIGHT] = "top_left_right_edges";
+        BORDER_MAP[BORDER_MIXED_TOP_BR] = "top_edge_br_corner";
+        BORDER_MAP[BORDER_MIXED_TOP_BL_BR] = "top_edge_bottom_corners";
+        BORDER_MAP[BORDER_MIXED_TOP_RIGHT_BL] = "top_right_edge_bl_corner";
+        BORDER_MAP[BORDER_CORNER_TR] = "one_corner";
+        BORDER_MAP[BORDER_CORNERS_TL_TR] = "two_corners";
+        BORDER_MAP[BORDER_CORNERS_BL_TR] = "opposite_corners";
+        BORDER_MAP[BORDER_CORNERS_BL_TR_BR] = "three_corners";
+        BORDER_MAP[BORDER_CORNERS_ALL] = "all_corners";
+    }
+    
+    public static final TextureLayoutMap BORDER_LAYOUT = TextureLayoutMap.create(TextureLayout.BORDER_13, (s, v, i) -> s + "_" + BORDER_MAP[i]);
+}
